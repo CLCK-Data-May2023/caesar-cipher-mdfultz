995c6f05da4d758a999f074fb508fdff5fc3dd5c
mdfultz <129699601+mdfultz@users.noreply.github.com>
2023-08-06 19:54:51 +0000

    Corrected uppercase

--- cipher.py
+++ cipher.py
@@ -6,21 +6,15 @@ cipher_shift = 5
 final_message =''
 
 #user input message
-starting_message = input('Please enter a senctence: ')
+starting_message = input('Please enter a sentence: ')
 
 #encode
 for character in starting_message:
     #check for special character
     if character.isalpha():
-        shift_letter = ord(character) + cipher_shift
-        #adjust value for upper case letter
-        if character.isupper():
-            if shift_letter > 90:
-                shift_letter -=26
-        #adjust value for lower case letter
-        else:
-            if shift_letter > 122:
-                shift_letter -=26
+        shift_letter = ord(character.lower()) + cipher_shift
+        if shift_letter > 122:
+            shift_letter -=26
         #append to final output if letter
         final_message += chr(shift_letter)
     else:
